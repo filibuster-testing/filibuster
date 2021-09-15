4ae25e4579d131c62eac65ce28718f91e4b7db1f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-09-15 18:21:59 -0400

    provide a default instrumentation file.

--- cli.py
+++ cli.py
@@ -1,13 +1,19 @@
+import os
 import click
+from os.path import abspath
 from filibuster.server import start_filibuster_server
 
 
 @click.command()
 @click.option('--functional-test', required=True, type=str, help='Functional test to run.')
-@click.option('--analysis-file', required=True, type=str, help='Analysis file.')
+@click.option('--analysis-file', default="default-instrumentation.json", type=str, help='Analysis file.')
 def test(functional_test, analysis_file):
     """Test a microservice application using Filibuster."""
-    start_filibuster_server(functional_test, analysis_file)
+
+    # Resolve full path of analysis file.
+    abs_analysis_file = abspath(os.path.dirname(os.path.realpath(__file__)) + "/" + analysis_file)
+
+    start_filibuster_server(functional_test, abs_analysis_file)
 
 
 if __name__ == '__main__':
