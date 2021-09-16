7fb038868a0c66015abf2bf221a6c1e5227b3121
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-09-16 12:58:43 -0400

    formatting.

--- filibuster/server_helpers/__init__.py
+++ filibuster/server_helpers/__init__.py
@@ -20,7 +20,8 @@ def load_counterexample(path):
     try:
         f = open(path)
         counterexample = json.load(f)
-        info("Counterexample found. Running the following counterexample: {}.".format(counterexample['functional_test']))
+        info(
+            "Counterexample found. Running the following counterexample: {}.".format(counterexample['functional_test']))
         f.close()
     except IOError:
         expected_file_at = "{}/{}".format(os.getcwd(), COUNTEREXAMPLE_PATH)
