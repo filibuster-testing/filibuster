48adec3b7abb763fb81d7c70094aca560300c791
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-09-24 09:50:25 -0400

    Cleanup documentation for loadgen.

--- filibuster_loadgen_cli.py
+++ filibuster_loadgen_cli.py
@@ -10,9 +10,11 @@ from filibuster.server import start_filibuster_server_and_run_multi_threaded_tes
 @click.option('--counterexample-file', required=True, type=str, help='Counterexample file to reproduce.')
 @click.option('--concurrency', required=True, type=int, help='Number of concurrent load generators.')
 @click.option('--num-requests', required=True, type=int, help='Number of requests for each load generator.')
-@click.option('--max-request-latency-for-failure', type=float, help='Maximum request latency before request is considered failure (seconds.)')
+@click.option('--max-request-latency-for-failure',
+              type=float,
+              help='Maximum request latency before request is considered failure (seconds.)')
 def loadgen(functional_test, analysis_file, counterexample_file, concurrency, num_requests, max_request_latency_for_failure):
-    """Test a microservice application using Filibuster."""
+    """Generate load for a given counterexample."""
 
     # Resolve full path of analysis file.
     abs_analysis_file = abspath(os.path.dirname(os.path.realpath(__file__)) + "/" + analysis_file)
