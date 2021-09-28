72ba06e51223832e2252bf1c6471657899dcddcb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-09-28 15:54:15 -0400

    add timeout, default to false if server not there.

--- filibuster/assertions/__init__.py
+++ filibuster/assertions/__init__.py
@@ -1,26 +1,31 @@
 import requests
 
+from filibuster.logger import error
+
 FILIBUSTER_HOST = "127.0.0.1"
 FILIBUSTER_PORT = "5005"
+TIMEOUT = 10
 
 
 def was_fault_injected():
     uri = "http://{}:{}/fault-injected".format(FILIBUSTER_HOST, FILIBUSTER_PORT)
-    response = requests.get(uri)
+    response = requests.get(uri, timeout=TIMEOUT)
 
     if response.status_code == 200:
         response_json = response.json()
         return response_json['result']
     else:
-        raise Exception("Cannot connect to the Filibuster server.")
+        error("Returning false from was_fault_injected, could not contact Filibuster server.")
+        return False
 
 
 def was_fault_injected_on(service_name):
     uri = "http://{}:{}/fault-injected/{}".format(FILIBUSTER_HOST, FILIBUSTER_PORT, service_name)
-    response = requests.get(uri)
+    response = requests.get(uri, timeout=TIMEOUT)
 
     if response.status_code == 200:
         response_json = response.json()
         return response_json['result']
     else:
-        raise Exception("Cannot connect to the Filibuster server.")
+        error("Returning false from was_fault_injected_on, could not contact Filibuster server.")
+        return False
