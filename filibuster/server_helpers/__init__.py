c7e1b0fa7a8069563881779a7337912f2b7f424f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2022-07-13 18:08:29 -0400

    Alter fault injection to allow percentage of faults.

--- filibuster/server_helpers/__init__.py
+++ filibuster/server_helpers/__init__.py
@@ -1,9 +1,22 @@
 import json
+from random import uniform
 
 from filibuster.logger import info, debug
 
-
 def should_fail_request_with(request, requests_to_fail):
+    return should_fail_request_with(request, requests_to_fail, 100)
+
+def should_fail_request_with(request, requests_to_fail, failure_percentage):
+    info("FP: " + str(failure_percentage))
+
+    if failure_percentage is not None and failure_percentage != 100:
+        # TODO: should we seed?
+        random_number = uniform(0, 100)
+        info("Failure Percentage: " + str(failure_percentage))
+        info("Uniform random: " + str(random_number))
+        if random_number > failure_percentage: # not <=
+            return None
+
     debug("Request: \n" + str(request))
     debug("Requests to fail: \n" + str(requests_to_fail))
     for request_to_fail in requests_to_fail:
